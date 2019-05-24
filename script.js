c99abbeb34dc142c713b40d7610db43b4cecb154
perryc85 <cjperry85@outlook.com>
2019-05-23 23:03:53 -0400

    another commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let i = 0;
+let images = [];
+let time = 5000;
+
+//image list 
+images[0] = "/images/images.png";
+images[1] = "/images/school.jpg";
+
+
+//change image
+function changePic(){
+    document.slide.src = images[i];
+    if(i < images.length - 1){
+        i++;
+    }else{
+        i = 0;
+    }
+
+    setTimeout("changePic()", time);
+}
+window.onload = changePic();
